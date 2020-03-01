fed362633d405ac6d086f42782dd861aa2c49ad2
Sergio Ruiz <sergiotto@outlook.com>
2020-03-01 16:53:53 +0100

    basic CenterTable component

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,32 @@
       <b-row>
         <Numbers :numbers="numbers" :date="date"></Numbers>
       </b-row>
+      <b-row>
+        <b-col cols="9">
+          <CenterTable :table="table"></CenterTable>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
 import axios from "axios"; // For the api-rest query
-import Numbers from './components/Numbers' //component to show winning numbers
+import Numbers from "./components/Numbers"; //component to show winning numbers
+import CenterTable from "./components/CenterTable"; // Central table width data
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Numbers
+    Numbers,
+    CenterTable
   },
   data() {
     return {
       numbers: [],
-      date: {}
+      date: {},
+      tableContent: {},
+      table: []
     };
   },
   mounted() {
@@ -35,12 +44,19 @@ export default {
         this.numbers = response.data.last.numbers.concat(
           response.data.last.euroNumbers
         );
+        this.tableContent = response.data.last.odds;
         this.date = response.data.last.date;
+
+        for (let i in this.tableContent) {
+          this.tableContent[i].prize = this.tableContent[i].prize;
+          this.tableContent[i].winners = this.tableContent[i].winners;
+          this.table.push(this.tableContent[i]);
+        }
       })
       .catch(error => {
         console.log(error);
       })
       .finally(() => (this.loading = false));
   }
-}
+};
 </script>
---
+++ src/components/CenterTable.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="principal">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Tier</th>
+          <th>Match</th>
+          <th>Winners</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in table" :key="item" class="linea">
+          <td class="tier">{{getRomanNumber(index)}}</td>
+          <td class="match">{{getText(index)}}</td>
+          <td class="winners">{{item.winners}}x</td>
+          <td class="amount">€ {{item.prize}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tableContent: Object,
+    table: []
+  },
+
+  methods: {
+      // Get roman number
+    getRomanNumber(index) {
+      let romansNumbers = [
+        "I",
+        "II",
+        "III",
+        "IV",
+        "V",
+        "VI",
+        "VII",
+        "VIII",
+        "IX",
+        "X",
+        "XI",
+        "XII"
+      ];
+      return romansNumbers[index];
+    },
+    // Text for the first column
+    getText(index) {
+      let text = [
+        "5 Numbers + 2 Euronumbers",
+        "5 Numbers + 1 Euronumber",
+        "5 Numbers + 0 Euronumbers",
+        "4 Numbers + 2 Euronumbers",
+        "4 Numbers + 1 Euronumber",
+        "4 Numbers + 0 Euronumber",
+        "3 Numbers + 2 Euronumbers",
+        "2 Numbers + 2 Euronumbers",
+        "3 Numbers + 1 Euronumber",
+        "3 Numbers + 0 Euronumbers",
+        "1 Number + 2 Euronumbers",
+        "2 Numbers + 1 Euronumber"
+      ];
+      return text[index];
+    }
+  }
+};
+</script>
