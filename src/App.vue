b754a3e3d8231afdf76f1cf8eaabbc4f4a53780f
Sergio Ruiz <sergiotto@outlook.com>
2020-03-01 16:39:03 +0100

    Numbers component + Axios + Bootstrap-Vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Starting lottoland test"/>
+    <b-container>
+      <b-row>
+        <div class="banner">
+          <h2>EUROJACKPOT RESULTS & WINNING NUMBERS</h2>
+        </div>
+      </b-row>
+      <b-row>
+        <Numbers :numbers="numbers" :date="date"></Numbers>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"; // For the api-rest query
+import Numbers from './components/Numbers' //component to show winning numbers
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Numbers
+  },
+  data() {
+    return {
+      numbers: [],
+      date: {}
+    };
+  },
+  mounted() {
+    axios
+      .get("https://www.lottoland.com/api/drawings/euroJackpot")
+      .then(response => {
+        this.numbers = response.data.last.numbers.concat(
+          response.data.last.euroNumbers
+        );
+        this.date = response.data.last.date;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => (this.loading = false));
   }
 }
 </script>
---
+++ src/components/Numbers.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <p>EuroJackpot Results for {{getWeekDay()}} {{date.day}} {{addCero(date.month)}} {{date.year}}</p>
+      </b-row>
+
+      <b-row>
+        <ul>
+          <li
+            v-for="(number, index) in numbers"
+            :key="index"
+            class="lotery-number"
+            :class="{'lotery-number-border' : index >= 5 }"
+          >{{number}}</li>
+        </ul>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Numbers",
+  props: {
+    numbers: Array, 
+    date: {} 
+  },
+  methods: {
+    // Add extra digit to a two character string
+    addCero(str) {
+      str = str.toString();
+      if (str.length < 2) {
+        str = "0" + str;
+      }
+      return str;
+    },
+    // To get the day of the week
+    getWeekDay() {
+      let d = new Date();
+      let weekDays = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+      return weekDays[d.getDay()];
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.lotery-number,
+.lotery-number-border {
+  display: inline;
+  border-radius: 50%;
+  display: inline-block;
+  font-size: 17px;
+  line-height: 35px;
+  text-align: center;
+  border: 1px solid #d9deda;
+  border-radius: 50%;
+  margin-right: 7px;
+  margin-bottom: 7px;
+  width: 35px;
+  height: 35px;
+}
+/* Changing color for the last two numbers */
+.lotery-number-border {
+  border: 1px solid #a35a15;
+}
+
+p {
+  margin-left: 17px;
+  font-size: 18px;
+}
+</style>
